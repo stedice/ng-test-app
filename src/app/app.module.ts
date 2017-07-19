c3c5ea05e9401d26587c768f2ec407bfcd6e3242
pizzafritta <stefanodc@mela2.local>
2017-07-19 22:51:35 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { AlbumlistComponent } from './albumlist/albumlist.component';
+import { PhotolistComponent } from './photolist/photolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlistComponent,
+    AlbumlistComponent,
+    PhotolistComponent
   ],
   imports: [
     BrowserModule
